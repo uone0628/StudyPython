9539bcd9835068d65d02489693c00575e23c437e
uone0628 <yu.want.coding@gmail.com>
2024-10-10 19:21:59 +0900

    tap 설명

---
+++ Tutoring/20241010.py
@@ -0,0 +1,36 @@
+def greet(name):
+    if name:
+        print(f"안녕하세요, {name}님!")
+    else:
+        print("이름을 입력해 주세요.")
+
+greet("홍길동")
+
+
+# 리스트의 각 요소를 출력하는 예시
+fruits = ['사과', '바나나', '체리']
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit)
+    print(fruit)
+    for fruit in fruits:
+        print(fruit)
+print(fruit)
+
+
+
+x=1
+v=1
+Z=1
+# while x<10: {
+#     print (x, "단 시작")
+#     while y<10:
+#         while z<10:
+#             print（x， "x"，y，"y， z，"="，xxy*z）
+#             Z=Z+1
+#         y=y+1
+#         Z=1
+#     ×+=1
+#     y=1
+# }
